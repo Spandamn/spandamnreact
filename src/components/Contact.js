f21e746b4b71a9cb2894db8c72fd5c6199a265f7
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2021-09-24 16:06:56 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ function Contact () {
     return (<section className="sec5" id="section5" data-scroll-index={4} data-anchor="page5">
     <div className="pp-tableCell">
       <div className="social">
-        <a href="https://www.linkedin.com/in/spandan-punwatkar-b602891a3/" title="LinkedIn"><img src={"../assets/lilogo.png"} title="LinkedIn" alt="LinkedIn" width={60} height={51} /></a><a href="https://www.instagram.com/spandn" title="Instagram"><img src="../assets/iglogo.png" alt="Instagram" width={60} height={60} /></a><a href="https://github.com/Spandamn" title="Github"><img src="../assets/GitHub-Mark-64px.png" alt="Github" width={60} height={60} /></a><a href="../assets/CV.pdf" title="My CV (PDF)"><img src="../assets/doc.png" alt="CV" width={60} height={60} /></a><a href="mailto:spendamn@gmail.com" title="Email Address"><img src="../assets/gmail.png" alt="Email" width={60} height={60} /></a>
+        <a href="https://www.linkedin.com/in/spandan-punwatkar-b602891a3/" title="LinkedIn"><img src={"../assets/lilogo.png"} title="LinkedIn" alt="LinkedIn" width={60} height={51} /></a><a href="https://www.instagram.com/spandn" title="Instagram"><img src="../assets/iglogo.png" alt="Instagram" width={60} height={60} /></a><a href="https://github.com/Spandamn" title="Github"><img src="../assets/GitHub-Mark-64px.png" alt="Github" width={60} height={60} /></a><a href="../assets/resume.pdf" title="My Resume (PDF)"><img src="../assets/doc.png" alt="Resume" width={60} height={60} /></a><a href="mailto:spendamn@gmail.com" title="Email Address"><img src="../assets/gmail.png" alt="Email" width={60} height={60} /></a>
       </div>
     </div>
   </section>);
